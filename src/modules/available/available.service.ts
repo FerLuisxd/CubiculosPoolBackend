555734a4ad7770fbad8ca597ea54c6da9a9192e3
Luis Vilca Flores <ferluisxd@gmail.com>
2020-05-03 00:56:57 -0500

    fix(insert)

--- src/modules/available/available.service.ts
+++ src/modules/available/available.service.ts
@@ -46,6 +46,9 @@ export class AvailableService {
         let closingWorkingHours = moment().tz("America/Lima").set({ hour:closingHours })  > moment().tz("America/Lima").set({ minute: 0, second: 0, millisecond: 0 })
         console.log(workingHours, closingWorkingHours)
         if(  workingHours && closingWorkingHours  ){
+            let timeToDelete = moment().tz("America/Lima").add(-1,'hour').set({ minute: 0, second: 0, millisecond: 0 }).toISOString()
+            console.log('timeToDelete', timeToDelete)
+            this.availableModel.deleteOne({"start": timeToDelete })
             let find = await this.availableModel.findOne({"start" : moment().tz("America/Lima").add(1,'day').set({ minute: 0, second: 0, millisecond: 0 }).toISOString()})
             if(!find){
                 console.log('corriendo')
@@ -57,9 +60,7 @@ export class AvailableService {
                 }
                 console.log(objToInsert)
                this.availableModel.insertMany([objToInsert])
-               let timeToDelete = moment().tz("America/Lima").add(-1,'hour').set({ minute: 0, second: 0, millisecond: 0 }).toISOString()
-               console.log('timeToDelete', timeToDelete)
-               this.availableModel.deleteOne({"start": timeToDelete })
+
             }
           
         }
